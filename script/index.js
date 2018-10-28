c2f4091674c6889c3076d4e5932cc35d8221baf5
Vladimir <shues@bk.ru>
2018-10-28 13:44:45 +0300

    28.10

--- script/index.js
+++ script/index.js
@@ -22,13 +22,11 @@ function add_tr() {
     new_string.appendChild(col_1);
     new_string.appendChild(col_2);
     new_string.appendChild(col_3);
+    new_string.className = "full_influsion";
     
     document.getElementsByTagName("tbody")[0].appendChild(new_string);
 
-    task_circle = document.getElementById("task_circle");
-    influsion_circle = document.getElementById("influsion_circle");
-
-    add_radius(task_circle);
+    update_circles();
 }
 
 
@@ -70,6 +68,19 @@ function blur_spisok(){
 function set_influsion(par){
     par.textContent = par.children[0].value;
     par.onclick = change_my_influsion;
+    switch(par.textContent){
+        case 'Влияю':
+            par.parentElement.className = "full_influsion";
+            break;
+        case 'Влияю косвенно':
+            par.parentElement.className = "half_influsion";
+            break;
+        case 'Не влияю':
+            par.parentElement.className = "non_influsion";
+            break;
+        default:
+            null;
+    }
 }
 
 function change_my_influsion(){
@@ -110,7 +121,37 @@ function change_comment(){
     this.onclick = null;        // Очищаем событие по клику на поле
 }
 
-function add_radius(circ){
-    var col_str = docment.getElementsByTagName("td");
-    circ.style.width = col_str.length() * 10 + "px";
+function update_circles(){
+    var full_influsion = document.getElementsByClassName("full_influsion").length;
+    var half_influsion = document.getElementsByClassName("half_influsion").length;
+    var non_influsion = document.getElementsByClassName("non_influsion").length;
+    
+    var inf_circle = document.getElementById("full_influsion_circle");
+    var half_inf_circle = document.getElementById("half_influsion_circle");
+    var non_inf_circle = document.getElementById("non_influsion_circle");
+    
+    inf_circle.style.width = full_influsion * 10  + "px";
+    inf_circle.style.height = full_influsion * 10  + "px";
+    inf_circle.style.borderRadius = (full_influsion * 10)/2  + "px";
+    
+    half_inf_circle.style.width = full_influsion * 10 + 50 + "px";
+    half_inf_circle.style.height = full_influsion * 10 + 50 + "px";
+    half_inf_circle.style.borderRadius = (full_influsion * 10)/2 + 50 + "px";
+    
+    non_inf_circle.style.width = full_influsion * 10 + 100 + "px";
+    non_inf_circle.style.height = full_influsion * 10 + 100 + "px";
+    non_inf_circle.style.borderRadius = (full_influsion * 10)/2 + 100 + "px";
+    
+    var non_inf_circle_diametr = non_inf_circle.clientHeight;
+    var half_inf_circle_diametr = half_inf_circle.clientHeight;
+    var inf_circle_diametr = inf_circle.clientHeight;
+    
+    var half_inf_circle_offset = (non_inf_circle_diametr - half_inf_circle_diametr)/2;
+    var inf_circle_offset = (half_inf_circle_diametr - inf_circle_diametr)/2;
+    
+    half_inf_circle.style.right = 10 + half_inf_circle_offset + "px";
+    half_inf_circle.style.top = 10 + half_inf_circle_offset + "px";
+    
+    inf_circle.style.right = 10 + inf_circle_offset + half_inf_circle_offset + "px";
+    inf_circle.style.top = 10 + inf_circle_offset + half_inf_circle_offset + "px";
 }
